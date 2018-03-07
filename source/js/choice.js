c3bfd5551362d38303ab30ae1b0ccd095d75a4c8
Andrey Bulaev <bulayka89@gmail.com>
2018-03-07 16:35:11 +0300

    Добавлен скрипт для отрисовки галочек

---
+++ source/js/choice.js
@@ -0,0 +1,8 @@
+// var link = document.querySelectorAll(".products__color-item"), i;
+//
+// for (i = 0; i < link.length; ++i) {
+//   link[i].addEventListener("click", function (event) {
+//     event.preventDefault(event);
+//     link.classList.add("active-color");
+//   })
+// };
