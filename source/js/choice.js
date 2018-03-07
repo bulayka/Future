3816b8f99da2794021131fb31f9e66040d50d962
Andrey Bulaev <bulayka89@gmail.com>
2018-03-07 22:32:03 +0300

    Доработан скрипт для выбора цвета (задание 2).

--- source/js/choice.js
+++ source/js/choice.js
@@ -1,8 +1,13 @@
-// var link = document.querySelectorAll(".products__color-item"), i;
-//
-// for (i = 0; i < link.length; ++i) {
-//   link[i].addEventListener("click", function (event) {
-//     event.preventDefault(event);
-//     link.classList.add("active-color");
-//   })
-// };
+var link = document.querySelectorAll(".products__color-item"), i;
+var q = document.querySelectorAll(".products__color-item"), i;
+for (i = 0; i < link.length; i++) {
+  link[i].addEventListener("click", function (event) {
+    event.preventDefault(event);
+    for (var j = 0; j < q.length; j ++) {
+      if (q[j].classList.contains("active-color")) {
+        q[j].classList.remove("active-color");
+      }
+    }
+    event.target.closest(".products__color-item").classList.add("active-color");
+  })
+};
